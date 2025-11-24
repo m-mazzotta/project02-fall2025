47d82fb4f9aa6b2ca543a6704332100f17fca0bf
m-mazzotta <mmazzotta0225@gmail.com>
2025-11-24 15:14:48 -0500

    starter frontend

---
+++ ui/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './globals.css';
+
+export default function App() {
+    return (
+        <>
+            <header>
+                <h1>TODO List</h1>
+            </header>
+            <main>
+                <p>Your app will go here!</p>
+            </main>
+        </>
+    );
+}
